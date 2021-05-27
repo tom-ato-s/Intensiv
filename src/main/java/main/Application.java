13ea09f219f6dfbb8bb71a53f2d3f7a4e78c2efd
Nadejda <mezz@74.ru>
2021-05-27 18:05:26 +0500

    css окончен, с недочетом в окне нов сообщения

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -3,8 +3,6 @@ package main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-;
-
 
 @SpringBootApplication
 public class Application {
